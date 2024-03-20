7bf87cb7afaf85531c82f080ae64c4e7004c1947
gustavool1 <go8895806@gmail.com>
2024-03-20 11:42:50 -0300

    feat: creating company and users tables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { dataSource } from './database';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  await dataSource.initialize();
+  await dataSource.runMigrations();
+
   await app.listen(3000);
 }
 bootstrap();
