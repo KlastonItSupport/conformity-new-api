7fce81582694ee63bc6f29da9b401a2a296e9659
gustavool1 <go8895806@gmail.com>
2024-05-14 12:36:17 -0300

    feat: adding response logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import Database from './database';
@@ -6,6 +6,8 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './modules/users/users.module';
 import { CompaniesModule } from './modules/companies/companies.module';
 import { PermissionsModule } from './modules/permissions/permissions.module';
+import { ErrorLoggingMiddleware } from './middlewares/error-logs/error-logs';
+import { ErrorLogsModule } from './middlewares/error-logs/error-logs.module';
 
 @Module({
   imports: [
@@ -14,8 +16,13 @@ import { PermissionsModule } from './modules/permissions/permissions.module';
     UsersModule,
     CompaniesModule,
     PermissionsModule,
+    ErrorLogsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ErrorLoggingMiddleware],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ErrorLoggingMiddleware).forRoutes('*');
+  }
+}
--- src/migrations/1713272724036-alteringUsersTable.ts
+++ src/migrations/1713272724036-alteringUsersTable.ts
@@ -2,28 +2,32 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AlteringUsersTable1713272724036 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const query = `
-ALTER TABLE users 
-  ADD celphone VARCHAR(255) NULL,
-  MODIFY birthday VARCHAR(255) NULL,
-  MODIFY access_rule VARCHAR(255) NULL,
-  MODIFY project_access BOOLEAN NULL,
-  MODIFY lead_access BOOLEAN NULL,
-  MODIFY contract_access BOOLEAN NULL;
-`;
+    const hasCelphoneColumn = await queryRunner.hasColumn('users', 'celphone');
+    if (!hasCelphoneColumn) {
+      await queryRunner.query(
+        `ALTER TABLE users ADD COLUMN celphone VARCHAR(255) NULL`,
+      );
+    }
 
-    await queryRunner.query(query);
+    await queryRunner.query(`
+      ALTER TABLE users
+      MODIFY birthday VARCHAR(255) NULL,
+      MODIFY access_rule VARCHAR(255) NULL,
+      MODIFY project_access BOOLEAN NULL,
+      MODIFY lead_access BOOLEAN NULL,
+      MODIFY contract_access BOOLEAN NULL;
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    const query = ` ALTER TABLE users
+    await queryRunner.query(`
+    ALTER TABLE users
     DROP COLUMN celphone,
-    MODIFY birthday VARCHAR(255) NULL,  -- Assuming varchar length of 255
+    MODIFY birthday VARCHAR(255) NULL,
     MODIFY access_rule VARCHAR(255) NULL,
     MODIFY project_access TINYINT NULL,
     MODIFY lead_access TINYINT NULL,
     MODIFY contract_access TINYINT NULL;
-    `;
-    await queryRunner.query(query);
+  `);
   }
 }
