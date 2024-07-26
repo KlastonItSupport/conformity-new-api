fa520972d5308325ec29c4fb8055fbe5644f9998
gustavool1 <go8895806@gmail.com>
2024-07-26 10:07:50 -0300

    feat: done origin routes

--- src/modules/tasks/controllers/origins.controller.ts
+++ src/modules/tasks/controllers/origins.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { OriginsServices } from '../services/origins.services';
 import { AuthGuard } from 'src/guards/auth.guard';
 
@@ -8,13 +19,33 @@ export class OriginsController {
 
   @Get()
   @UseGuards(AuthGuard)
-  getOrigins(@Req() req) {
-    return this.originsService.getOrigins(req.user.companyId);
+  getOrigins(@Req() req, @Query() data) {
+    return this.originsService.getOrigins(req.user.companyId, {
+      page: data.page,
+      pageSize: data.pageSize,
+      search: data.search,
+    });
   }
 
   @Post()
   @UseGuards(AuthGuard)
   async createOrigin(@Body() data, @Req() req) {
     return this.originsService.createOrigin(data.name, req.user.companyId);
+  }
+
+  @Patch(':id')
+  @UseGuards(AuthGuard)
+  updateOrigin(@Body() data, @Req() req) {
+    return this.originsService.updateOrigin(
+      data.id,
+      data.name,
+      req.user.companyId,
+    );
+  }
+
+  @Delete(':id')
+  @UseGuards(AuthGuard)
+  deleteOrigin(@Req() req, @Param('id') id) {
+    return this.originsService.deleteOrigin(id, req.user.companyId);
   }
 }
--- src/modules/tasks/services/origins.services.ts
+++ src/modules/tasks/services/origins.services.ts
@@ -13,7 +13,59 @@ export class OriginsServices {
     private readonly companiesRepository: Repository<Company>,
   ) {}
 
-  async getOrigins(companyId: string) {
+  async getOrigins(
+    companyId: string,
+    search?: { search?: string; page?: number; pageSize?: number },
+  ) {
+    if ((search && search.page) || search.pageSize || search.search) {
+      const queryBuilder = this.originsRepository.createQueryBuilder('origins');
+
+      if (search.search) {
+        const searchParam = `%${search.search || ''}%`;
+
+        queryBuilder.andWhere((qb) => {
+          qb.where('origins.name LIKE :searchParam', { searchParam });
+        });
+      }
+
+      queryBuilder.andWhere('origins.origin_company_fk = :companyId', {
+        companyId,
+      });
+
+      if (search.page && search.pageSize) {
+        queryBuilder
+          .offset((search.page - 1) * search.pageSize)
+          .limit(search.pageSize);
+      }
+
+      const pagination = {};
+      const origins = await queryBuilder.getManyAndCount();
+      const totalOrigins = origins[1];
+      const lastPage = search.pageSize
+        ? Math.ceil(totalOrigins / search.pageSize)
+        : 1;
+
+      const links = {
+        first: 1,
+        last: lastPage,
+        next: search.page + 1 > lastPage ? lastPage : search.page + 1,
+        totalPages: search.pageSize
+          ? Math.ceil(totalOrigins / search.pageSize)
+          : 1,
+        currentPage: search.pageSize ? search.page : 1,
+        previous: search.pageSize
+          ? search.page > 1
+            ? search.page - 1
+            : 0
+          : null,
+        totalItems: totalOrigins,
+      };
+
+      pagination['items'] = origins[0];
+      pagination['pages'] = links;
+
+      return pagination;
+    }
     const company = await this.companiesRepository.findOne({
       where: { id: companyId },
     });
@@ -33,5 +85,39 @@ export class OriginsServices {
     });
 
     return await this.originsRepository.save(origin);
+  }
+
+  async updateOrigin(id: number, name: string, companyId: string) {
+    const company = await this.companiesRepository.findOne({
+      where: { id: companyId },
+    });
+
+    const origin = await this.originsRepository.findOne({
+      where: { id, company },
+    });
+
+    if (!origin) {
+      throw new Error('Origin not found');
+    }
+
+    origin.name = name;
+
+    return await this.originsRepository.save(origin);
+  }
+
+  async deleteOrigin(id: number, companyId: string) {
+    const company = await this.companiesRepository.findOne({
+      where: { id: companyId },
+    });
+
+    const origin = await this.originsRepository.findOne({
+      where: { id, company },
+    });
+
+    if (!origin) {
+      throw new Error('Origin not found');
+    }
+
+    return await this.originsRepository.remove(origin);
   }
 }
